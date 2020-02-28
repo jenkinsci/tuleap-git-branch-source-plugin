c7ac6878515eb0ea9dd85263210251d99a427748
Martin Goyot <martin.goyot@enalean.com>
2020-02-28 12:28:54 +0100

    Check support of JCAsC
    This change is part of [community #14019](https://tuleap.net/plugins/tracker/?aid=14019)
    
    This commit introduces a test suite that check that our plugin is able
    to load its configuration through JCAsC.

---
+++ src/test/java/org/jenkinsci/plugins/tuleap_git_branch_source/jcasc/JCAsCTest.java
@@ -0,0 +1,44 @@
+package org.jenkinsci.plugins.tuleap_git_branch_source.jcasc;
+
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import hudson.security.ACL;
+import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
+import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+import org.jenkinsci.plugins.tuleap_credentials.TuleapAccessToken;
+import org.jenkinsci.plugins.tuleap_git_branch_source.config.TuleapConfiguration;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+
+public class JCAsCTest {
+
+    @ClassRule
+    @ConfiguredWithCode("jenkins.yml")
+    public static JenkinsConfiguredWithCodeRule jenkins = new JenkinsConfiguredWithCodeRule();
+
+    @Test
+    public void itShouldImportTheTuleapAccessToken() {
+        final List<TuleapAccessToken> tokens = CredentialsProvider.lookupCredentials(
+            TuleapAccessToken.class,
+            jenkins.jenkins,
+            ACL.SYSTEM,
+            Collections.emptyList()
+        );
+
+        assertThat(tokens, hasSize(1));
+        assertThat(tokens.get(0).getToken().getPlainText(), is("tlp-k1-24.2dd47d2e9280a430f4eb862cfbc85e90e8b286818dcbe85bd60d8feb7162580a"));
+    }
+
+    @Test
+    public void itShouldImportTheTuleapServerConfiguration() {
+        final TuleapConfiguration configuration = TuleapConfiguration.get();
+
+        assertThat(configuration.getDomainUrl(), is("https://my.tuleap.instance.net"));
+    }
+}
